e2eb96a033af6f514ca9c11ab4425ddaf183dffd
sid2983 <sid24000576@gmail.com>
2024-08-19 23:44:30 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from typing import List
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()     
+   
+
+__version__ = "0.0.4"
+REPO_NAME = "mongoconnectorpkg"
+PKG_NAME= "databaseautomation"
+AUTHOR_USER_NAME = "sid2983"
+AUTHOR_EMAIL = "sid24000576@gmail.com"
+
+setup(
+    name=PKG_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A python package for connecting with database.",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    )
+
