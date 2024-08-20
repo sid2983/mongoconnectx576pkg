19884bf9b29346c39c06940c2b9c78211a362935
sid2983 <sid24000576@gmail.com>
2024-08-20 12:29:25 +0530

    packagename updated

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # mongoconnectx576pkg
+This is a testing package for connecting to MongoDB 3.6.6 using the pymongo package.
--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+from setuptools import find_packages
 from typing import List
 
 with open('README.md', 'r', encoding='utf-8') as f:
